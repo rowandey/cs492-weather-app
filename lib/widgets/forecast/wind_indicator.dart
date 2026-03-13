743dcf71f008495d37f8ee794349d4e208517ebc
Rowan Dey <deyro@oregonstate.edu>
2026-03-12 17:57:33 -0700

    added smooth animation

--- lib/widgets/forecast/wind_indicator.dart
+++ lib/widgets/forecast/wind_indicator.dart
@@ -1,8 +1,21 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weatherapp/providers/forecast_provider.dart';
 
 import 'package:weatherapp/models/forecast.dart';
+
+class ShortestAngleTween extends Tween<double> {
+  ShortestAngleTween({required double end}) : super(begin: end, end: end);
+
+  @override
+  double lerp(double t) {
+    double diff = (end! - begin!);
+    // force it to rotate the short way around
+    diff = (diff + math.pi) % (2 * math.pi) - math.pi;
+    return begin! + diff * t;
+  }
+}
 
 class WindIndicator extends StatelessWidget {
   const WindIndicator({
@@ -15,13 +28,20 @@ class WindIndicator extends StatelessWidget {
     String windDirection = forecastProvider.activeForecast!.windDirection;
     // String windSpeed = forecastProvider.activeForecast!.windSpeed;
 
+    final angle = getAngleFromWindDirection(windDirection);
+
     return Column(
       children: [
         Text("Wind Direction"),
-        SizedBox(
-          width: 100,
-          height: 100,
-          child: CustomPaint(painter: WindLineDrawer(getAngleFromWindDirection(windDirection))),
+        TweenAnimationBuilder<double>(
+          tween: ShortestAngleTween(end: angle),
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          builder: (context, value, _) => SizedBox(
+            width: 100,
+            height: 100,
+            child: CustomPaint(painter: WindLineDrawer(value)),
+          ),
         ),
       ],
     );
